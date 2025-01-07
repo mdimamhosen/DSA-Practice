ca838114201da4f5da8e5795edbb0f022871d56a
Md Imam Hosen <mimam22.cse@bu.ac.bd>
2025-01-07 15:27:39 +0600

    CPP CodeForces Practise

--- CF Practise/A_Jagged_Swaps.cpp
+++ CF Practise/A_Jagged_Swaps.cpp
@@ -2,15 +2,30 @@
 using namespace std;
 
 int main() {
-
     int t;
     cin >> t;
-    while(t--){
-       int n;
-       vector<int> v(n);
-         for(int i=0; i<n; i++){
-              cin >> v[i];
-         }
+    while (t--) {
+        int n;
+        cin >> n;
+        vector<int> v(n);
+        for (int i = 0; i < n; i++) {
+            cin >> v[i];
+        }
+
+        vector<int> sorted_v = v;
+        sort(sorted_v.begin(), sorted_v.end());
+
+        for (int i = 1; i < n - 1; i++) {
+            if (v[i] > v[i - 1] && v[i] > v[i + 1]) {
+                swap(v[i], v[i + 1]);
+            }
+        }
+
+        if (v == sorted_v) {
+            cout << "YES" << endl;
+        } else {
+            cout << "NO" << endl;
+        }
     }
 
     return 0;
